82ebdcdbefd0f35e08fb0050461fed7d9b9d05fc
Polina <polina.solodkova@gmail.com>
2025-09-26 15:13:00 +0300

    fix: add mock dependencies

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleNameMapper: {
+    '\\.css$': 'identity-obj-proxy'
+  },
+};
